d37e33e0837f01604c95d6ce1673fc6889d1b6e2
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-01-11 12:14:38 +0900

    css をファイルに書く

--- qa-easy-ask-layer.php
+++ qa-easy-ask-layer.php
@@ -8,68 +8,13 @@ class qa_html_theme_layer extends qa_html_theme_base
         if ($this->template==='easy-ask') {
             $styles =<<<EOS
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.css">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
-            <style>
-
-            .file-input {
-                display: none;
-            }
-
-            .md-button label {
-                display: inline-block;
-                width: 100%;
-                cursor: pointer;
-            }
-
-            .wrap-border {
-                border: 1px solid #ddd;
-            }
-        
-            .logo img {
-                width: 150px;
-            }
-        
-            .text-size-110 {
-                font-size: 110%;
-            }
-        
-            .photo-upload.error {
-                border-width: 2px;
-                border-color: rgb(211, 47, 47);
-            }
-        
-            md-radio-button {
-                border: 1px solid #aaa;
-                border-radius: 3px;
-                padding: 8px;
-                min-width: 120px;
-            }
-        
-            md-radio-button.error {
-                background: rgb(183, 28, 28);
-                border-color: rgb(183, 28, 28);
-            }
-        
-            md-radio-button:hover,
-            md-radio-button.md-checked {
-                border-color: rgba(255, 171, 64, 0.87);
-                background: rgba(255, 171, 64, 0.2);
-            }
-        
-            md-radio-button .md-container {
-                left: 8px;
-            }
-        
-            @media (max-width: 374px) and (min-width: 0) and (orientation: portrait) {
-                .md-toolbar-tools {
-                height: 76px;
-                max-height: 76px;
-                }
-            }
-            </style>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
 EOS;
             $this->output($styles);
+            $css = '';
+            $this->output('<LINK REL="stylesheet" TYPE="text/css" HREF="'.QA_HTML_THEME_LAYER_URLTOROOT.'css/easy-ask.css"/>');
+            $this->output($css);
         }
     }
 
@@ -80,7 +25,7 @@ EOS;
             $form_template = QEA_DIR.'/html/form_'.$form_id.'.html';
             if (file_exists($form_template)) {
                 $tmpl = file_get_contents($form_template);
-                $url = qa_path_to_root().basename( QA_PLUGIN_DIR ).DIRECTORY_SEPARATOR.QEA_FOLDER;
+                $url = QA_HTML_THEME_LAYER_URLTOROOT;
                 $html = strtr($tmpl, array(
                     '^url' => $url
                 ));
