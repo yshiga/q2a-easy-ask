9e940692ad6c88c570c12b8e86a46c7cfb0a5567
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-01-11 11:09:51 +0900

    plugin file add

---
+++ qa-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+    Plugin Name: Q2A Easy Ask
+    Plugin URI:
+    Plugin Description: Easy Ask Form for Q2A.
+    Plugin Version: 1.0
+    Plugin Date: 2018-01-11
+    Plugin Author: 38qa.net
+    Plugin Author URI:
+    Plugin License: GPLv2
+    Plugin Minimum Question2Answer Version: 1.7
+    Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+    header('Location: ../../');
+    exit;
+}
+
+/*
+    Omit PHP closing tag to help avoid accidental output
+*/
