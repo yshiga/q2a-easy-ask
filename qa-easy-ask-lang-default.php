86b9804eb3cc82321a078637aafa7c76cc8de7cb
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-01-15 11:41:02 +0900

    js 用に翻訳追加

--- qa-easy-ask-lang-default.php
+++ qa-easy-ask-lang-default.php
@@ -2,5 +2,18 @@
 
 // default(Japanese)
 return array(
-    'no_login_message' => '<p>このページは会員限定です。<br>すでに会員登録済みですか？ ログインは<a href="/login">こちら</a><br>未登録ですか？会員登録は<a href="/register">こちら</a></p>',
+    'no_login_message'    => '<p>このページは会員限定です。<br>すでに会員登録済みですか？ ログインは<a href="/login">こちら</a><br>未登録ですか？会員登録は<a href="/register">こちら</a></p>',
+    'confirm_title'       => '質問を投稿しますか？',
+    'confirm_content'     => 'この内容で質問を投稿しますか？内容を変更する場合は、いいえを押して入力内容を変更してください。投稿した質問は、後から編集することができます。',
+    'confirm_ok'          => '投稿する',
+    'confirm_cancel'      => 'いいえ',
+    'question1_title'     => '捕獲用の巣箱はどこにおけばいいですか？',
+    'question1_place'     => '場所の概要',
+    'question1_owned'     => '自分の土地ですか？',
+    'question1_wind'      => '風が強いですか？',
+    'question1_sunlight'  => '直射日光が強く当たる場所ですか？',
+    'question1_pesticide' => '農薬の散布がされる場所ですか？',
+    'question1_others'    => '人が近づくような場所ですか？',
+    'comment'             => 'コメント',
+    'question_footer'     => 'この質問は、簡単質問フォームで投稿されました。',
 );
